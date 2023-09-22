05e4b6537151ab2cc33f684122e77e0f404915ec
BirtErik <erikbj3@gmail.com>
2023-09-22 16:21:50 +0200

    Initial commit

---
+++ src/main/java/com/hivetech/taskmanager/user/model/User.java
@@ -0,0 +1,34 @@
+package com.hivetech.taskmanager.user.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
+
+
